c3c4b2b60ec0c49958eced15ccb595d1213a82cc
Alexandre Hamez <199517+ahamez@users.noreply.github.com>
2024-12-17 21:51:13 +0100

    Problem 2095

---
+++ cpp/2095_delete_middle_node.cc
@@ -0,0 +1,98 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+#include <cassert>
+#include <iostream>
+
+struct ListNode
+{
+  int val;
+  ListNode* next;
+
+  ListNode()
+      : ListNode{0, nullptr}
+  {
+  }
+  ListNode(int x)
+      : ListNode{x, nullptr}
+
+  {
+  }
+  ListNode(int x, ListNode* next)
+      : val(x),
+        next(next)
+  {
+  }
+};
+
+class Solution
+{
+ public:
+  ListNode* deleteMiddle_rec(ListNode* head)
+  {
+    assert(head != nullptr);
+    return recurse(head, head);
+  }
+
+  ListNode* deleteMiddle(ListNode* head)
+  {
+    assert(head != nullptr);
+
+    if (head->next == nullptr)
+    {
+      return nullptr;
+    }
+
+    auto slow = head;
+    auto fast = head;
+    auto prev = head;
+
+    while (fast != nullptr and fast->next != nullptr)
+    {
+      prev = slow;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    prev->next = slow->next;
+
+    return head;
+  }
+
+ private:
+  ListNode* recurse(ListNode* slow, ListNode* fast)
+  {
+    assert(slow != nullptr);
+    assert(fast != nullptr);
+
+    if (fast->next == nullptr)
+    {
+      return slow->next;
+    }
+    else if (fast->next->next == nullptr)
+    {
+      slow->next = slow->next->next;
+      return slow;
+    }
+    else
+    {
+      auto ret = recurse(slow->next, fast->next->next);
+      slow->next = ret;
+      return slow;
+    }
+  }
+};
+
+int main()
+{
+  const auto l0 = new ListNode{1, new ListNode{2, new ListNode{3, new ListNode{4}}}};
+
+  auto r = Solution{}.deleteMiddle(l0);
+  while (r)
+  {
+    std::cout << r->val << ", ";
+    r = r->next;
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
