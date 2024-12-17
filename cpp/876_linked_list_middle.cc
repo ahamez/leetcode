b2d13316dc787fbe89c528a585039aeac8fdbd99
Alexandre Hamez <199517+ahamez@users.noreply.github.com>
2024-12-17 20:32:31 +0100

    Problem 876

---
+++ cpp/876_linked_list_middle.cc
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+#include <cassert>
+
+struct ListNode
+{
+  int val;
+  ListNode* next;
+
+  ListNode()
+      : ListNode{0, nullptr}
+  {
+  }
+  ListNode(int x)
+      : ListNode{x, nullptr}
+
+  {
+  }
+  ListNode(int x, ListNode* next)
+      : val(x),
+        next(next)
+  {
+  }
+};
+
+class Solution
+{
+ public:
+  ListNode* middleNode(ListNode* head)
+  {
+    assert(head != nullptr);
+    return recurse(head, head);
+  }
+
+ private:
+  ListNode* recurse(ListNode* slow, ListNode* fast)
+  {
+    assert(slow != nullptr);
+    assert(fast != nullptr);
+
+    if (fast->next == nullptr)
+    {
+      return slow;
+    }
+    else if (fast->next->next == nullptr)
+    {
+      return slow->next;
+    }
+    else
+    {
+      return recurse(slow->next, fast->next->next);
+    }
+  }
+};
+
+int main()
+{
+  const auto l0 = new ListNode{3, new ListNode{4, new ListNode{5}}};
+  const auto l1 = new ListNode{1, new ListNode{2, l0}};
+
+  assert(Solution{}.middleNode(l1) == l0);
+
+  return 0;
+}
