c0b07150a4de82f9ad862e906b8305d69a698f39
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-10 22:12:49 +0100

    Problem 125

---
+++ cpp/125_valid_palindrome.cc
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <cctype>
+#include <iterator>
+#include <string>
+
+class Solution
+{
+ public:
+  bool isPalindrome(std::string s)
+  {
+    auto s_prime = std::string{};
+    s_prime.reserve(s.size());
+
+    for (auto c : s)
+    {
+      if (std::isalnum(c))
+      {
+        s_prime.push_back(std::tolower(c));
+      }
+    }
+
+    auto head = cbegin(s_prime);
+    auto tail = crbegin(s_prime);
+
+    while (head != cend(s_prime))
+    {
+      if (*head != *tail)
+      {
+        return false;
+      }
+      ++head;
+      ++tail;
+    }
+
+    return true;
+  }
+};
+
+int main()
+{
+  assert(Solution{}.isPalindrome("A man, a plan, a canal: Panama") == true);
+  assert(Solution{}.isPalindrome("race a car") == false);
+  assert(Solution{}.isPalindrome(" ") == true);
+  return 0;
+}
