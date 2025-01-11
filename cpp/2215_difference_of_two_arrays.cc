6da7626fea028e0826bac73a4b2bb43e98fed530
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-11 23:23:01 +0100

    Problem 2215: add a faster variant

--- cpp/2215_difference_of_two_arrays.cc
+++ cpp/2215_difference_of_two_arrays.cc
@@ -12,20 +12,43 @@ class Solution
  public:
   std::vector<std::vector<int>> findDifference(std::vector<int> xs, std::vector<int> ys)
   {
-    const auto xs_set = std::unordered_set<int>{cbegin(xs), cend(xs)};
-    const auto ys_set = std::unordered_set<int>{cbegin(ys), cend(ys)};
+    // Version 1. Sort inputs and remove duplicates. Fastest version.
+    std::ranges::sort(xs);
+    const auto new_xs_end = std::unique(begin(xs), end(xs));
+    xs.erase(new_xs_end, cend(xs));
+
+    std::ranges::sort(ys);
+    const auto new_ys_end = std::unique(begin(ys), end(ys));
+    ys.erase(new_ys_end, cend(ys));
 
     auto xs_not_in_ys = std::vector<int>{};
     xs_not_in_ys.reserve(xs.size());
     auto ys_not_in_xs = std::vector<int>{};
     ys_not_in_xs.reserve(ys.size());
 
-    std::ranges::copy_if(xs_set, std::back_inserter(xs_not_in_ys), [&](auto x) { return not ys_set.contains(x); });
-    std::ranges::copy_if(ys_set, std::back_inserter(ys_not_in_xs), [&](auto y) { return not xs_set.contains(y); });
+    std::ranges::set_difference(xs, ys, std::back_inserter(xs_not_in_ys));
+    std::ranges::set_difference(ys, xs, std::back_inserter(ys_not_in_xs));
 
     return {xs_not_in_ys, ys_not_in_xs};
 
-    // Version using ordered sets and set_difference. Seems to be a little slower.
+    // Version 2. Uses unordered sets. Slighty faster than version 3.
+
+    // const auto xs_set = std::unordered_set<int>{cbegin(xs), cend(xs)};
+    // const auto ys_set = std::unordered_set<int>{cbegin(ys), cend(ys)};
+
+    // auto xs_not_in_ys = std::vector<int>{};
+    // xs_not_in_ys.reserve(xs.size());
+    // auto ys_not_in_xs = std::vector<int>{};
+    // ys_not_in_xs.reserve(ys.size());
+
+    // std::ranges::copy_if(xs_set, std::back_inserter(xs_not_in_ys), [&](auto x) { return not ys_set.contains(x); });
+    // std::ranges::copy_if(ys_set, std::back_inserter(ys_not_in_xs), [&](auto y) { return not xs_set.contains(y); });
+
+    // return {xs_not_in_ys, ys_not_in_xs};
+
+    // --------------------------------------------------------------------
+    // Version 3. Uses ordered sets and set_difference. A little slower than version 2.
+
     // const auto xs_set = std::set<int>{cbegin(xs), cend(xs)};
     // const auto ys_set = std::set<int>{cbegin(ys), cend(ys)};
 
